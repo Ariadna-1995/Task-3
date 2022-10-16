4d7e629d51b62b4b2112925eebeeab7b614e1265
ARIADNA <rariadna@inbox.ru>
2022-10-16 21:54:56 +0300

    Solution with code corrections

--- Program.cs
+++ Program.cs
@@ -3,9 +3,9 @@ Console.Write("Введите число ");
 int num = int.Parse(Console.ReadLine()!);
 if(num % 2 == 0)
 {
-    Console.Write(${num} - четное число);
+    Console.Write($"{num} - четное число");
 }
 else
 {
-    Console.Write(${num} - нечетное число);
+    Console.Write($"{num} - нечетное число");
 }
